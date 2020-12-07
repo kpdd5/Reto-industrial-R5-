e9a8cb2b24f94bcf3fda020e20d49fcda7d28406
karen <karenkarenpaoh@hotmail.com>
2020-12-07 12:40:47 -0500

    archivos

--- README.md
+++
@@ -1,2 +0,0 @@
-# Retos_de_la_Industria_GrupoR5
-Realizar el Checkout para agregar productos a la compra del SOAT
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
-import { Router } from "@reach/router"
+ import { Router } from "@reach/router"
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Register from '../pages/Register';
+import Home from '../pages/Home';
+import Pagos from '../pages/Pagos';
+import Layout from '../components/Layout';
+import MetodoPago from '../components/MetodoPago';
+import TuCarro from '../components/TuCarro';
+import Ofertas from '../components/Ofertas';
+const App = () => (
+    <BrowserRouter>
+    <Layout>
+     <Switch>
+      <Route exact path="/" component={Home}/>
+      
+      <Route exact path="/Register" component={Register}/>
+      <Route exact path="/Pagos" component={Pagos}/>
+      <Route exact path="/MetodoPago" component={MetodoPago}/>
+      <Route exact path="/TuCarro" component={TuCarro}/>
+      <Route exact path="/Ofertas" component={Ofertas} />
+     </Switch>
+    </Layout>
+    </BrowserRouter>
+);
 
-import Register from '../pages/Register'
-import Home from '../pages/Home'
-
-
-
-const App = () => {
-return(
-  <Router>
-        <Home path="/"/>
-        <Register  path="/register"/>
-  </Router>
-)
-}
-
-export default App
-
+export default App;
